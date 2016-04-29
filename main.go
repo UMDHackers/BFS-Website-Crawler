24d0257f6873e5108c618d9fba516fc4cf209855
Parth Desai <PDesai@FCMD.local>
2016-04-29 15:25:07 -0400

    made post results

--- main.go
+++ main.go
@@ -11,13 +11,20 @@ type Results struct {
 type Index struct {
 
 }
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   i := Index{}
   tem, _ := template.ParseFiles("html/index.html")
   tem.Execute(w, i)
 }
-
+func resultsHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("this is the message: ", r.Method)
+  if r.Method == "GET" {
+    http.Redirect(w, r, "/", 301)
+  } else {
+    r.ParseForm()
+    fmt.Println("this is the url ", r.Form["url"])
+  }
+}
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
     w.WriteHeader(status)
     if status == http.StatusNotFound {
@@ -26,7 +33,7 @@ func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 }
 func main() {
 	http.HandleFunc("/", indexHandler)
-	// http.HandleFunc("/results/", resultsHandler)
+	http.HandleFunc("/results", resultsHandler)
   http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
   http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
 
